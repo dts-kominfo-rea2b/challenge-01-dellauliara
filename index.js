635cc5f0531b78ca13d092706b8b12ddeedd9558
dellauliara <diella.aulia@gmail.com>
2022-05-26 12:58:47 +0700

    melengkapi tugas, education masih berupa [object]

--- index.js
+++ index.js
@@ -1,11 +1,135 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+//Monica's Favorite Color
+let favoriteColorMonica = new Set();
+favoriteColorMonica.add("Yellow");
+favoriteColorMonica.add("Pink");
+favoriteColorMonica.add("White");
+favoriteColorMonica.add("Purple");
+//Wendy's Favorite Color
+let favoriteColorWendy = new Set();
+favoriteColorWendy.add("Blue");
+favoriteColorWendy.add("Black");
+favoriteColorWendy.add("Grey");
+//isHavePet
+let isHavePet = new Map();
+isHavePet.set('Monica','Yes');
+isHavePet.set('Wendy','No');
+//education
+//  function education(name, city, graduate){
+//      this.name = name;
+//      this.city = city;
+//      this.graduate = graduate;
+    
+//  }
+ const eduMonica = {
+     name: "SD 01",
+     city: "Jakarta",
+     graduate: "2016"
+}
+ const eduMonica2 = {
+    name: "SMP 02",
+    city: "Jakarta",
+    graduate: "2019"
+} 
+const eduMonica3 = {
+    name: "SMA 03",
+    city: "Tangerang"
+}
+ 
+ const educationMonica = [];
+ educationMonica.push(eduMonica);
+ educationMonica.push(eduMonica2);
+ educationMonica.push(eduMonica3);
 
+// const educationMonica = [];
+// educationMonica.push(JSON.parse(JSON.stringify(eduMonica)));
+// educationMonica.push(JSON.parse(JSON.stringify(eduMonica2)));
+// educationMonica.push(JSON.parse(JSON.stringify(eduMonica3)));
+
+// Wendy's Education
+const eduWendy = {
+    name: "SD 02",
+    city: "Jakarta",
+    graduate: "2016"
+}
+const eduWendy2 = {
+   name: "SMP 03",
+   city: "Jakarta",
+   graduate: "2019"
+} 
+const eduWendy3 = {
+   name: "SMA 04",
+   city: "Tangerang"
+}
+const eduWendy4 = {
+    name: "Universitas",
+    city: "Tangerang"
+ }
+
+const educationWendy = [];
+educationWendy.push(eduWendy);
+educationWendy.push(eduWendy2);
+educationWendy.push(eduWendy3);
+educationWendy.push(eduWendy4);
+
+
+//Monica's Favorite Restaurant
+let favoriteRestaurantMonica = new Set();
+favoriteRestaurantMonica.add("Bento");
+favoriteRestaurantMonica.add("Sushi");
+favoriteRestaurantMonica.add("Pancake");
+favoriteRestaurantMonica.add("Eggy");
+favoriteRestaurantMonica.add("Tempura");
+favoriteRestaurantMonica.add("Bento");
+favoriteRestaurantMonica.add("Eggy");
+favoriteRestaurantMonica.add("Padang");
+favoriteRestaurantMonica.add("tteok");
+favoriteRestaurantMonica.add("Sushi");
+favoriteRestaurantMonica.add("Sushi");
+
+//Wendy's Favorite Restaurant
+let favoriteRestaurantWendy = new Set();
+favoriteRestaurantWendy.add("Tempura");
+favoriteRestaurantWendy.add("Bento");
+favoriteRestaurantWendy.add("Sushi");
+favoriteRestaurantWendy.add("Pancake");
+favoriteRestaurantWendy.add("Padang");
+favoriteRestaurantWendy.add("Katsu");
+favoriteRestaurantWendy.add("Geprek");
+favoriteRestaurantWendy.add("Pancake");
+favoriteRestaurantWendy.add("Eggy");
+
+
+const firstUser = {
+    name: "Monica",
+    age:"17",
+    email:"monica@dingdong.com",
+    favoriteColor: favoriteColorMonica,
+    isHavePet: isHavePet.get('Monica'),
+    education: educationMonica,
+    favoriteRestaurant: favoriteRestaurantMonica
+};
+
+console.log(firstUser);
+
+const secondUser = {
+    name: "Wendy",
+    age: "23",
+    email:"wendy@dingdong.com",
+    favoriteColor: favoriteColorWendy,
+    isHavePet: isHavePet.get('Wendy'),
+    education: educationWendy,    
+    favoriteRestaurant: favoriteRestaurantWendy,
+    
+};
+
+console.log(secondUser);
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
+users.push(firstUser);
+users.push(secondUser);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
